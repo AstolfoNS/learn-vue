dbdedf21cb4d4c0de340f33ff7938b52c9182fe2
Astolfo <1780884916@qq.com>
2025-05-20 00:21:24 +0800

    完成了demo4的练习

--- demo4/main.js
+++ demo4/main.js
@@ -1,12 +1,25 @@
 const app = {
   data() {
     return {
-      text: "Hello Vue!",
+      perspective: 200,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
     };
   },
   methods: {
-    submit() {
-      this.text = "Hello World!";
+    handleReset() {
+      this.perspective = 200;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+  },
+  computed: {
+    boxStyle() {
+      return {
+        transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
+      };
     },
   },
 };
